6af2cef16439c4a27b251b0a08d129fdd454401f
Mehmet Yüksel POLAT <mypolat@outlook.com>
2020-12-19 17:22:27 +0300

    static db name changed with env db name

--- src/services/user-service/db/config/config.js
+++ src/services/user-service/db/config/config.js
@@ -1,26 +1,26 @@
-const asd = require('dotenv').config({path: __dirname + '/../../.env'});
-const { DB_HOST, DB_USERNAME, DB_PASSWORD } = process.env;
+const asd = require("dotenv").config({ path: __dirname + "/../../.env" });
+const { DB_HOST, DB_DATABASE, DB_USERNAME, DB_PASSWORD } = process.env;
 
 module.exports = {
-  "development": {
-    "username": DB_USERNAME,
-    "password": DB_PASSWORD,
-    "database": "ms_user_dev",
-    "host": DB_HOST,
-    "dialect": "postgres"
+  development: {
+    username: DB_USERNAME,
+    password: DB_PASSWORD,
+    database: DB_DATABASE + "_dev",
+    host: DB_HOST,
+    dialect: "postgres",
   },
-  "test": {
-    "username": DB_USERNAME,
-    "password": DB_PASSWORD,
-    "database": "ms_user_test",
-    "host": DB_HOST,
-    "dialect": "postgres"
+  test: {
+    username: DB_USERNAME,
+    password: DB_PASSWORD,
+    database: DB_DATABASE + "_test",
+    host: DB_HOST,
+    dialect: "postgres",
   },
-  "production": {
-    "username": DB_USERNAME,
-    "password": DB_PASSWORD,
-    "database": "ms_user_prod",
-    "host": DB_HOST,
-    "dialect": "postgres"
-  }
+  production: {
+    username: DB_USERNAME,
+    password: DB_PASSWORD,
+    database: DB_DATABASE + "_prod",
+    host: DB_HOST,
+    dialect: "postgres",
+  },
 };
