f79caefcffcc2efc7dd9595dca82e6580b5b61fc
Mehmet Yüksel POLAT <mypolat@outlook.com>
2020-12-20 14:58:46 +0300

    static values changed to dot env values

--- src/api-gateway/app.js
+++ src/api-gateway/app.js
@@ -1,18 +1,26 @@
-const { gql, ApolloServer } = require("apollo-server");
-const { ApolloGateway }  = require('@apollo/gateway');
+const { ApolloServer } = require("apollo-server");
+const { ApolloGateway } = require("@apollo/gateway");
 
 const PORT = process.env.PORT || 4000;
 
+const USER_SERVICE =
+  process.env.SERVICE_USER_HOST + ":" + process.env.SERVICE_USER_PORT ||
+  "http://localhost:3001";
+
+const REWARD_SERVICE =
+  process.env.SERVICE_REWARD_HOST + ":" + process.env.SERVICE_REWARD_PORT ||
+  "http://localhost:3002";
+
 const server = new ApolloServer({
-    gateway: new ApolloGateway({
-      serviceList: [
-        { name: 'userService', url: 'http://localhost:3000/' },
-        { name: 'rewardService', url: 'http://localhost:3001/' }
-      ]
-    }), 
-    subscriptions: false
-  }); 
-  
-  server.listen(PORT).then(({ url }) => {
-    console.log(`Server ready at ${url}`);
-  });
+  gateway: new ApolloGateway({
+    serviceList: [
+      { name: "userService", url: USER_SERVICE },
+      { name: "rewardService", url: REWARD_SERVICE },
+    ],
+  }),
+  subscriptions: false,
+});
+
+server.listen(PORT).then(({ url }) => {
+  console.log(`Server ready at ${url}`);
+});
