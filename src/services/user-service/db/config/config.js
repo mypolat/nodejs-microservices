6caf5ef8d414be5c6df832672e20c977b16ab9c4
Mehmet Yüksel POLAT <mypolat@outlook.com>
2020-12-20 19:51:03 +0300

    dockercompose fixed

--- src/services/user-service/db/config/config.js
+++ src/services/user-service/db/config/config.js
@@ -1,26 +1,25 @@
-require("dotenv").config({ path: __dirname + "/../../.env" });
 const { DB_HOST, DB_DATABASE, DB_USERNAME, DB_PASSWORD } = process.env;
 
 module.exports = {
   development: {
-    username: DB_USERNAME,
-    password: DB_PASSWORD,
-    database: DB_DATABASE + "_dev",
-    host: DB_HOST,
+    username: DB_USERNAME || "postgres",
+    password: DB_PASSWORD || "123456",
+    database: DB_DATABASE || "ms_user" + "_dev",
+    host: DB_HOST || "127.0.0.1",
     dialect: "postgres",
   },
   test: {
-    username: DB_USERNAME,
-    password: DB_PASSWORD,
-    database: DB_DATABASE + "_test",
-    host: DB_HOST,
+    username: DB_USERNAME || "postgres",
+    password: DB_PASSWORD || "123456",
+    database: DB_DATABASE || "ms_user" + "_test",
+    host: DB_HOST || "127.0.0.1",
     dialect: "postgres",
   },
   production: {
-    username: DB_USERNAME,
-    password: DB_PASSWORD,
-    database: DB_DATABASE + "_prod",
-    host: DB_HOST,
+    username: DB_USERNAME || "postgres",
+    password: DB_PASSWORD || "123456",
+    database: DB_DATABASE || "ms_user" + "_prod",
+    host: DB_HOST || "127.0.0.1",
     dialect: "postgres",
   },
 };
